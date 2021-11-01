27827c0e75bcac9a7285dedc9bfdf07651e2f22c
Guilherme Sperandio <guilhermesperandio@pecege.com>
2021-11-01 07:36:11 -0300

    Criação do dtMoney

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from "../Dashboard/styles";
+import {Summary} from "../Summary/index";
+import { TransactionsTable } from "../TransactionsTable";
+export function Dashboard(){
+  return(
+    <Container>
+        <Summary/>
+        <TransactionsTable/>
+    </Container>
+  )
+}
